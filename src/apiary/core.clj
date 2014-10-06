c21b33dd9313d849fb292fa4697eff989774f069
Jordan Schatz <jordan@noionlabs.com>
2014-10-06 10:59:08 -0700

    Add usage notes

--- src/apiary/core.clj
+++ src/apiary/core.clj
@@ -1,6 +1,22 @@
 (ns apiary.core
   (:require [apiary.github :as g]))
 
-;; Sample usage
+;; ──────────────────────────────────────────────────────────────────────
+;; Lets Go!
+;;
+;; - get this weeks issues
+;; - get this weeks score
+;; currently do nothing with them
+;;
+;; - get last weeks issues
+;; - get last weeks score
+;;
+;; Review issues:
+;; - Are there unscored issues?
+;; - Are there issues assigned to no-one?
+
+(def this-weeks-issues (g/summary (first (g/issues))))
+(def this-weeks-score (first (g/scores)))
+
+(def last-weeks-issues (g/summary (second (g/issues))))
 (def last-weeks-score (second (g/scores)))
-(def last-weeks-issue-summary (g/summary (second (g/issues))))
